12b2d32bdf2dec8a3b1b935ea6cf4116a151c752
Yakup Çatalkaya <31369965+yakupcatalkaya@users.noreply.github.com>
2020-12-05 23:03:36 +0300

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -5,17 +5,21 @@ Created on Tue Aug 18 02:22:54 2020
 @author: yakupcatalkaya
 """
 
+
 import pygame as pyg
 import csv
 import random
 import hashlib
 import time
+import os
+
 
 running = True
 id_num=""
 global_list=[]
 program_clock=time.time()
-   
+directory=os.getcwd()
+
 def get_windows_size():    
     width,height=pyg.display.Info().current_w,pyg.display.Info().current_h
     return width,height
@@ -42,10 +46,12 @@ def stimuli_scaler(size):
     width,height=size
     scale_w,scale_h=width/default_w,height/default_h
     return (scale_w,scale_h)
+
     
 def get_font(font_type="comicsansms",size=14):
     font = pyg.font.SysFont(font_type,size)
     return font
+
 
 def create_text(texts,color=(255, 255, 255),size=14,font_type="comicsansms"):
     size=int(size*((scale_x*scale_y)**(1/2)))
@@ -60,6 +66,7 @@ def show_stimuli(source,up=0,left=0):
     area=(-(int(left*scale_x))+int(w*scale_x)//2- x//2,-(int(up*scale_y))+int(h*scale_y)//2- y//2)
     screen.blit(source,area)
     
+
 def first_screen():
     text2=create_text(texts="Welcome to the experiment",size=54)
     text3=create_text(texts="The experiment is build by UMRAM Farooqui's Lab")
@@ -69,12 +76,14 @@ def first_screen():
     update_screen()
     pyg.time.delay(1000)
 
+
 def empty_screen():
     global background
     background = pyg.Surface(win_size)
     screen.fill(black)
     update_screen()
     
+
 def checking():
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
@@ -88,6 +97,7 @@ def checking():
             elif event.key == pyg.K_SPACE:
                 return False
 
+
 def start_and_scale():
     global scale_x,scale_y,win_size,screen,title,black,white,default_w,default_h
     pyg.init()    
@@ -96,36 +106,37 @@ def start_and_scale():
     black=(0,0,0)
     white=(255,255,255)
     default_w,default_h=1920,1080
-    # clock = pyg.time.Clock()
-    # clock.tick(60)
     win_size=get_windows_size()
-    # win_size=(1920,1080)
     screen = pyg.display.set_mode(win_size)
     screen = pyg.display.set_mode(win_size,pyg.FULLSCREEN)  #  (win_size,pyg.FULLSCREEN)   #(672,378)
     scale_x,scale_y=stimuli_scaler(win_size)
     
+
 def update_screen():
     pyg.display.update()
 
+
 def random_number_list():
-    vowel_list=[]
-    consonant_list=[]
-    consonants="B C D F G H J K L M N P Q R S T V W X Z".split()
-    vowels="A E I O U".split()
+    number_list=[]
+    letter_list=[]
+    letters="A B C D E F G H I K L M N P Q R S T V X Y Z".split()
+    numbers="1 2 3 4 5 6 7 8 9".split()
     for step in range(32):
         ran_num=random.randint(0,4)
-        vowel_list.append(vowels[ran_num])
+        number_list.append(numbers[ran_num])
     for step in range(121):
-        ran_num=random.randint(0,19)
-        consonant_list.append(consonants[ran_num])
-    templist=vowel_list+consonant_list
+        ran_num=random.randint(0,19) 
+        letter_list.append(letters[ran_num])
+    templist=number_list+letter_list
     random.shuffle(templist)
     atemplist=templist[:]
+    ##########################
     indexlist=list(range(9,153,9))
     count=0
     for index in indexlist:
         templist.insert(index+count,"gap")
         count+=1
+    
     indexlist=[]
     for step in range(16):
         index=random.randrange(1,151)
@@ -136,40 +147,53 @@ def random_number_list():
     for index in indexlist:
         atemplist.insert(index+count,"gap")
         count+=1
+        
     count=0
     for letter in templist:
         if count%10==0 and count!=0:
-            if letter in vowels:
+            if letter in numbers:
                 ran_num=random.randint(1,168)
-                while (templist[ran_num] in vowels) or templist[ran_num]=="gap" or ran_num%10==0:
+                while (templist[ran_num] in numbers) or templist[ran_num]=="gap" or ran_num%10==0:
                     ran_num=random.randint(1,168)
                 templist[count],templist[ran_num]=templist[ran_num],templist[count]
         count+=1 
+        
     count=0
     for letter in atemplist:
         if 0<count<len(atemplist)-1:
-            if (letter in vowels and atemplist[count-1]=="gap") or (letter=="gap" and (atemplist[count-1]=="gap" or atemplist[count+1]=="gap")):
+            if (letter in numbers and atemplist[count-1]=="gap") or (letter=="gap" and (atemplist[count-1]=="gap" or atemplist[count+1]=="gap")):
                 ran_num=random.randint(2,167)
-                while (atemplist[ran_num] in vowels) or atemplist[ran_num]=="gap" or (atemplist[ran_num+1]=="gap" or atemplist[ran_num-1]=="gap" or atemplist[ran_num-1] in vowels or atemplist[ran_num+1] in vowels):
+                while (atemplist[ran_num] in numbers) or atemplist[ran_num]=="gap" or (atemplist[ran_num+1]=="gap" or atemplist[ran_num-1]=="gap" or atemplist[ran_num-1] in numbers or atemplist[ran_num+1] in numbers):
                     ran_num=random.randint(2,167)
                 atemplist[count],atemplist[ran_num]=atemplist[ran_num],atemplist[count]
-        count+=1
-    
+        count+=1 
     return templist,atemplist
 
 
 def experiment_screen(ready=True):
+    count_session_regular=1
+    count_session_random=1
     global global_list
     exp1,exp2=random_number_list()
     score,error,miss=0,0,0
     timelist=[]
-    pyg.time.delay(1000)            
+    pyg.time.delay(1000)   
+ 
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            pyg.quit()
+        elif event.type == pyg.KEYDOWN:
+            if event.key == pyg.K_ESCAPE:
+                exit_screen()
+            elif event.key == pyg.K_SPACE:
+                break
     for item in exp1:
         if item=="gap":
+            count_session_regular+=1
             repeat=break_screen(typo="set")
+            if repeat>=300:repeat=300
             timelist.append(repeat)
             global_list.append(["gap","gap","gap","gap","gap",str(repeat*10)+" ms has been spend."])
-            pyg.time.delay(1000)
         else:
             done=False
             stimuli_start=time.time()
@@ -177,38 +201,49 @@ def experiment_screen(ready=True):
                 screen.fill(black)        
                 show_stimuli(create_text(texts=str(item),size=154))
                 update_screen()
-                pyg.time.delay(10)
                 state=is_true(item,1)
                 if state==True:
                     error+=1
+                    stimuli_stop=time.time()
+                    step=step
                     done=True
                     break
                 elif state==False:
                     score+=1
+                    stimuli_stop=time.time()
+                    step=step
                     done=True
                     break
-            if not done:
-                state=show_fixation(item)
-                if state==True:
-                    error+=1
-                elif state==False:
-                    score+=1
-                elif state=="None":
+            if done:
+                screen.fill(black)        
+                show_stimuli(create_text(texts=str(item),size=154))
+                update_screen()
+                pyg.time.delay(int(18-step)*10+1)
+                show_fixation(item,just=False)
+            elif not done:
+                for a in range(49):
+                    countiii=a
+                    state=show_fixation(item)
+                    if state==True:
+                        error+=1
+                        stimuli_stop=time.time()
+                        break
+                    elif state==False:
+                        score+=1
+                        stimuli_stop=time.time()
+                        break
+                if state=="None":
                     miss+=1
-                stimuli_stop=time.time()
-            else:
-                stimuli_stop=time.time()
-                show_fixation(item)
-            react_time=str(int((stimuli_stop-stimuli_start)*1000-10))+" ms "
-            if state=="None":
-                react_time="None"
-            global_list.append([item,state,error,score,miss,react_time])
+                    stimuli_stop=time.time()
+                pyg.time.delay((48-countiii)*10+1)
+            react_time=str(int((stimuli_stop-stimuli_start)*1000))   
+            global_list.append([item,state,error,score,miss,react_time,"regular",count_session_regular])
+  
     break_screen()
-    
-    
-    counti=0    
+    counti=0
     for item in exp2:
         if item=="gap":
+            count_session_random+=1
             for a in range(timelist[counti]):
                 screen.fill(black)
                 update_screen()
@@ -223,41 +258,59 @@ def experiment_screen(ready=True):
                 screen.fill(black)        
                 show_stimuli(create_text(texts=str(item),size=154))
                 update_screen()
-                pyg.time.delay(10)
                 state=is_true(item,1)
                 if state==True:
                     error+=1
+                    stimuli_stop=time.time()
+                    step=step
                     done=True
                     break
                 elif state==False:
                     score+=1
+                    stimuli_stop=time.time()
+                    step=step
                     done=True
                     break
-            if not done:
-                state=show_fixation(item)
-                if state==True:
-                    error+=1
-                elif state==False:
-                    score+=1
-                elif state=="None":
+            if done:
+                screen.fill(black)        
+                show_stimuli(create_text(texts=str(item),size=154))
+                update_screen()
+                pyg.time.delay(int(18-step)*10+1)
+                show_fixation(item,just=False)
+            elif not done:
+                for a in range(49):
+                    countiii=a
+                    state=show_fixation(item)
+                    if state==True:
+                        error+=1
+                        stimuli_stop=time.time()
+                        break
+                    elif state==False:
+                        score+=1
+                        stimuli_stop=time.time()
+                        break
+                if state=="None":
                     miss+=1
-                stimuli_stop=time.time()
-            else:
-                stimuli_stop=time.time()
-                show_fixation(item)
-            react_time=str(int((stimuli_stop-stimuli_start)*1000-10))+" ms "
-            global_list.append([item,state,error,score,miss,react_time])
-            
-            
-def show_fixation(item,colour=(255,255,255)):
+                    stimuli_stop=time.time()
+                pyg.time.delay((48-countiii)*10+1)
+            react_time=str(int((stimuli_stop-stimuli_start)*1000))   
+            global_list.append([item,state,error,score,miss,react_time,"regular",count_session_regular])
+                
+
+def show_fixation(item,colour=(255,255,255),just=True):
     screen.fill(black)        
     show_stimuli(create_text(texts=str("+"),color=colour,size=154))
     update_screen()
-    state=is_true(item,49)
-    return state
+    if not just:
+        pyg.time.delay(490)
+    if just:
+        state=is_true(item,1)
+        return state
     
+
 def is_true(item,onestimuli):
-    vowels="A E I O U".split()
+    letters="A B C D E F G H I K L M N P Q R S T V X Y Z".split()
+    # wait_time=0
     for trial in range(onestimuli):
         for event in pyg.event.get():
             if event.type == pyg.QUIT:
@@ -269,16 +322,15 @@ def is_true(item,onestimuli):
                     waiting()
                     continue
                 elif event.key == pyg.K_DOWN:
-                    if item in vowels:
-                        pyg.time.delay((onestimuli-trial)*10+1)
+                    if item in letters:
                         return True
                         break
                     else:
-                        pyg.time.delay((onestimuli-trial)*10+1)
                         return False
                         break
-        pyg.time.delay(10)
+        pyg.time.delay(7)
     return "None"
+
 
 def exit_screen():
     global global_list
@@ -300,13 +352,19 @@ def exit_screen():
         pyg.time.delay(95)
     pyg.quit()
 
+
 def break_screen(typo="session"):
     screen.fill(black)
-    show_stimuli(create_text(texts="Press button to everything except Vowels",size=54),up=50)
-    show_stimuli(create_text(texts="Press space to continue to "+str(typo),size=34),up=-50)
-    update_screen()
+    if typo=="session":
+        show_stimuli(create_text(texts="Another long session is going to start!",size=54),up=50)
+        show_stimuli(create_text(texts="Press space to continue to the long "+str(typo),size=34),up=-50)
+        update_screen()
+    else:
+        show_stimuli(create_text(texts="Press down arrow key button to everything except numbers",size=54),up=50)
+        show_stimuli(create_text(texts="Press space to continue to "+str(typo),size=34),up=-50)
+        update_screen()
     repeat=0
-    while True:    
+    while True:
         for event in pyg.event.get():
             if event.type == pyg.QUIT:
                 pyg.quit()
@@ -318,16 +376,23 @@ def break_screen(typo="session"):
                     break
         pyg.time.delay(9)
         repeat+=1
+        
 
 def instruction_screen():
     screen.fill(black)
-    show_stimuli(create_text(texts="The Instructions",size=54),up=200)
-    show_stimuli(create_text(texts="-------------------------------------------",size=54),up=160,left=0)
-    show_stimuli(create_text(texts="1)  In experiment, there are 17 set and in each set there are 9 stimuli.",size=24),up=100,left=10)
-    show_stimuli(create_text(texts="2)  Participant should press down arrow key when a consonant is appeared in the screen.",size=24),up=50,left=-85)
-    show_stimuli(create_text(texts="3)  Otherwise,do nothing. ",size=24),up=0,left=265)
-    show_stimuli(create_text(texts="4)  There are 10 sessions [5 from each type] in the experiment. ",size=24),up=-50,left=50)
-    show_stimuli(create_text(texts="5)  Press space button to continue the experiment . ",size=24),up=-100,left=120)
+    show_stimuli(create_text(texts="The Instructions",size=54),up=290)
+    show_stimuli(create_text(texts="-------------------------------------------",size=54),up=250,left=0)
+    show_stimuli(create_text(texts="1)  In experiment, there are 17 set and in each set there are 9 stimuli.",size=24),up=190,left=10)
+    show_stimuli(create_text(texts="2)  Participant should press down arrow key when a letter is appeared in the screen.",size=24),up=130,left=-65)
+    show_stimuli(create_text(texts="3)  For numbers,do nothing. ",size=24),up=70,left=250)
+    show_stimuli(create_text(texts="4)  There are 10 sessions [5 from each type] in the experiment. ",size=24),up=10,left=50)
+    show_stimuli(create_text(texts="5)  Press space button to continue the experiment . ",size=24),up=-50,left=120)
+    show_stimuli(create_text(texts="Talimatlar",size=54),up=-120)
+    show_stimuli(create_text(texts="-------------------------------------------",size=54),up=-180,left=0)
+    show_stimuli(create_text(texts="1) Ekranda bir harf gördüğünüzde aşağı ok tuşuna basınız.",size=24),up=-240,left=95)
+    show_stimuli(create_text(texts="2) Ekranda bir rakam gördüğünüzde hiçbir tuşa basmayınız.",size=24),up=-300,left=90)
+    show_stimuli(create_text(texts="3) Deney, kısa görevler ve uzun görevler olmak üzere toplam 10 seanstan oluşmaktadır.",size=24),up=-360,left=-65)
+    show_stimuli(create_text(texts="4) Lütfen devam etmek için boşluk tuşuna basınız.",size=24),up=-420,left=145)
     update_screen()
     while True:
         for event in pyg.event.get():
@@ -338,6 +403,7 @@ def instruction_screen():
                     exit_screen()
                 elif event.key == pyg.K_SPACE:
                     return None
+                
 
 def subject_sign():
     global signed,id_num
@@ -360,7 +426,6 @@ def subject_sign():
                     num=num[:-1]
                 else: 
                     num += event.unicode
-
         screen.fill(black)
         text1=create_text(texts='ENTER YOUR SUBJECT ID:',color=white,size=34)
         text3=create_text(texts='Then, press enter',color=white,size=34)
@@ -370,8 +435,10 @@ def subject_sign():
         show_stimuli(text3,up=-100,left=280)
         update_screen()
 
+
 def create_csv_file(global_list):
     global signed,id_num
+    print("The data file is at: "+str(directory))
     if signed is True:
         pass
     else:
@@ -392,6 +459,7 @@ def create_csv_file(global_list):
             line.append(checksum)          
             rw.writerow(line)
             
+
 def main():
     start_and_scale()
     subject_sign()
